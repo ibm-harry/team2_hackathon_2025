98f6575a84d457d3c0a64164a7c47425eea4dc92
Shaariq Muhammad Ali <muhammadalishaariq@gmail.com>
2025-04-02 14:40:32 +0100

    increased font size

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -3,8 +3,8 @@ import styles from "./Card.module.css";
 function Card({ title, content, imageUrl }) {
   return (
     <div className={styles.main}>
-      <h2>{title}</h2>
-      <p>{content}</p>
+      <h2 className={styles.heading}>{title}</h2>
+      <p className={styles.heading}>{content}</p>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import Card from "./components/Card";
 export default function Home() {
   return (
     <div className={styles.main}>
-      <Card title="Card Title" content="Card Content"></Card>
-      <Card title="Card Title" content="Card Content"></Card>
-      <Card title="Card Title" content="Card Content"></Card>
-      <Card title="Card Title" content="Card Content"></Card>
-      <Card title="Card Title" content="Card Content"></Card>
-      <Card title="Card Title" content="Card Content"></Card>
+      <Card title="Card Title 1" content="Card Content"></Card>
+      <Card title="Card Title 2" content="Card Content"></Card>
+      <Card title="Card Title 3" content="Card Content"></Card>
+      <Card title="Card Title 4" content="Card Content"></Card>
+      <Card title="Card Title 5" content="Card Content"></Card>
+      <Card title="Card Title 6" content="Card Content"></Card>
     </div>
   );
 }
