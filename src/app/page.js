167437a09a7e2d3303c79bdeab2ee56285c3e2ce
Shaariq Muhammad Ali <muhammadalishaariq@gmail.com>
2025-04-02 13:56:02 +0100

    updated casing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Card from "./components/card";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
