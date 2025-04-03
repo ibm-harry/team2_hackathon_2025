bd438ba4ea1aebfb2dbc4501cd15b55e1976c5ca
mohsinoibm <mohsin.osman-cic@ibm.com>
2025-04-03 13:50:10 +0100

    Adding toggle to hide comments

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,17 @@ import { useSocket } from "../../hooks/useSocket";
 import styles from './styles.module.css';
 
 export default function Home() {
-  const { slide, comments } = useSocket();
+  const { slide, comments, showComments, toggleComments } = useSocket();
+  
+  const VisibilityIcon = () => (
+    <svg viewBox="0 0 32 32" width="24" height="24" fill="currentColor">
+      {showComments ? (
+        <path d="M30.94,15.66A16.69,16.69,0,0,0,16,5,16.69,16.69,0,0,0,1.06,15.66a1,1,0,0,0,0,.68A16.69,16.69,0,0,0,16,27,16.69,16.69,0,0,0,30.94,16.34,1,1,0,0,0,30.94,15.66ZM16,25c-5.3,0-10.9-3.93-13.91-9C5.1,10.93,10.7,7,16,7s10.9,3.93,13.91,9C26.9,21.07,21.3,25,16,25Z M16,11a5,5,0,1,0,5,5A5,5,0,0,0,16,11Z"/>
+      ) : (
+        <path d="M30.94,15.66A16.69,16.69,0,0,0,16,5,16.69,16.69,0,0,0,1.06,15.66a1,1,0,0,0,0,.68A16.69,16.69,0,0,0,16,27,16.69,16.69,0,0,0,30.94,16.34,1,1,0,0,0,30.94,15.66ZM16,25c-5.3,0-10.9-3.93-13.91-9C5.1,10.93,10.7,7,16,7s10.9,3.93,13.91,9C26.9,21.07,21.3,25,16,25ZM16,11a5,5,0,1,0,5,5A5,5,0,0,0,16,11Zm0,8a3,3,0,1,1,3-3A3,3,0,0,1,16,19Z"/>
+      )}
+    </svg>
+  );
 
   const slides = [
     "/Frame 2.png"
@@ -24,7 +34,7 @@ export default function Home() {
                   index === slide ? styles.activeImage : styles.inactiveImage
                 }`}
               />
-              {comments
+              {showComments && comments
                 .filter(comment => comment.slideIndex === index)
                 .map(comment => (
                   <div
@@ -44,6 +54,15 @@ export default function Home() {
           </div>
         ))}
       </div>
+      <button
+        className={styles.commentButton}
+        onClick={toggleComments}
+      >
+        <span className={styles.buttonContent}>
+          <VisibilityIcon />
+          <span>{showComments ? 'Hide Comments' : 'Show Comments'}</span>
+        </span>
+      </button>
     </div>
   );
 }
