656e57797813151449488843dd3d536b6a402ab6
Harry Macklewain-Lawrence <harry.macklewain-lawrence-cic@ibm.com>
2025-04-03 21:56:18 +0100

    Removed margin from body.

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} style={{margin: 0}}>
         {children}
       </body>
     </html>
